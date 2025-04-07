83e53055b885178f8bb6073b83ae630463849f0b
vladimir <vladim1r.gribkov@yandex.ru>
2025-04-07 19:23:41 +0400

    get coords

---
+++ mobileModule02/medium_weather_app/app/hooks/useLocation.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+import * as Location from "expo-location";
+
+const useLocation = () => {
+    const [erroMsg, setErroMsg] = useState("");
+    const [longitude, setLongitude] = useState<number | null>(null);
+    const [latitude, setLatitude] = useState<number | null>(null);
+
+    const getUserLocation = async () => {
+        let {status} = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            setErroMsg('Permission denied granted');
+            return;
+        }
+
+        let {coords} = await Location.getCurrentPositionAsync();
+        if (coords) {
+            const { latitude, longitude } = coords;
+            setLatitude(latitude);
+            setLongitude(longitude);
+        }
+    }
+
+    useEffect(() => {
+        getUserLocation();
+    }, [])
+    return {latitude, longitude, erroMsg}
+}
+
+export default useLocation
---
+++ mobileModule02/medium_weather_app/app/weekly.tsx
@@ -0,0 +1,28 @@
+import {StyleSheet, Text, View} from "react-native";
+
+export default function Weekly({geolocation}: {geolocation: string}) {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <View style={styles.centerText} >
+                <Text style={styles.header}>Weekly</Text>
+                <Text style={styles.header}>{geolocation}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 25,
+    },
+    centerText: {
+        display: "flex",
+        alignItems: "center",
+    }
+})
